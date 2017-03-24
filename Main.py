d8000fab8e45e073492cbc564aa5b4dbd09760d3
Brad <brad@enkrona.net>
2017-03-24 10:53:08 -0500

    Updating for current version of project.

--- Main.py
+++ Main.py
@@ -8,7 +8,7 @@
 # Copyright 2009 Secons Ltd. (www.obdtester.com)                       #
 # Copyright 2009 Peter J. Creath                                       #
 # Copyright 2016 Brendan Whitfield (brendan-w.com)                     #
-# Copyright 2016 Bradley Taylor                                        #
+# Copyright 2016-2017 Bradley Taylor                                   #
 #                                                                      #
 ########################################################################
 #                                                                      #
@@ -41,7 +41,7 @@ connection = obd.OBD()  # auto-connects to USB or RF port
 
 # Commands we wish to utilize/query later on
 # Core/Base Info you would want on your dash
-cmd_MPH = obd.commands.SPEED  # select an OBD command (sensor) [Speed (I believe it is originally fed in KPH)]
+cmd_KPH = obd.commands.SPEED  # select an OBD command (sensor) [Speed (I believe it is originally fed in KPH)]
 cmd_RPM = obd.commands.RPM
 cmd_FUEL = obd.commands.FUEL_LEVEL
 
@@ -56,7 +56,9 @@ cmd_FUEL_RATE = obd.commands.FUEL_RATE
 # ------------------------------------------------------------------------------------------------------------------- #
 
 # Stores the query as an object that we can print, read from, etc
-in_MPH = connection.query(cmd_MPH)  # send the command, and parse the response
+""" These queries are a one time query and do NOT automatically update.
+    For auto update we must utilize a method that constantly is calling queries.   """
+in_KPH = connection.query(cmd_KPH)  # send the command, and parse the response
 in_RPM = connection.query(cmd_RPM)
 in_FUEL_LEVEL = connection.query(cmd_FUEL)
 in_TEMP_OUTSIDE = connection.query(cmd_TEMP_OUTSIDE)
@@ -64,29 +66,41 @@ in_OIL_TEMP = connection.query(cmd_OIL_TEMP)
 in_MPG = connection.query(cmd_FUEL_RATE)
 
 
+# print(in_MPH.value.to("mph"))
 # ------------------------------------------------------------------------------------------------------------------- #
 
 # Handy method to print CURRENT_VALUE (or CV) of the object we feed it -- note you must perform a query before feeding
 
 def printout_cv(query):
-    for i in range(1):  # Prints the first 5 values -- DEBUGGING TOOL
-        print(query.value)
-    return
+    print str(query)
+
+# ------------------------------------------------------------------------------------------------------------------- #
+
+# Conversion method from KPH to MPH
+
+def kph_to_mph(kph):
+    return kph * 0.621371
 
 
 # ------------------------------------------------------------------------------------------------------------------- #
+
 while in_RPM > 1:
-    printout_cv(in_RPM)  # Purely for Debugging ATM to test RPM Changes
-    printout_cv(in_MPH)
+    print ""
+    print str(in_RPM) + " RPMs"  # Purely for Debugging ATM to test RPM Changes
+    printout_cv(in_KPH)
     printout_cv(in_FUEL_LEVEL)
+    print ""
     # getting new values to return
-    in_MPH = connection.query(cmd_MPH)  # send the command, and parse the response
+    in_MPH = connection.query(cmd_KPH)  # send the command, and parse the response
     in_RPM = connection.query(cmd_RPM)
     in_FUEL_LEVEL = connection.query(cmd_FUEL)
+
 # print(response.value.to("mph"))  # user-friendly unit conversions  #! Throws an error will investigate in later build!
 
-# Very simply shuts down the Pi if the RPMs = 0, i.e. car is off & has been 3 secs
+# Very simply shuts down the Pi if the RPMs = 0, i.e. car is off & has been 3 secs #! dilapadated - will update in later
+# builds
 timeout = time.time() + 3.0
+print type(timeout)
 """
 if in_RPM == 0 & time.time() > timeout:
     subprocess.call("./shutdown.sh", shell=True)
